0dc14983c43f1ef022eae7d77879bdadd4ca7060
Tomasz Waszczyk <tomasz.waszczyk@relayr.io>
2021-01-09 17:26:23 +0100

    Tests tests passing variables

--- passing/src/main.rs
+++ passing/src/main.rs
@@ -1,14 +1,28 @@
+use std::{usize};
+
 fn main() {
     println!("Hello, world!");
 
-    // passByValue("Tomek test");
-    my_other_function("Tomek test")
+    // let mut testString: std::string::String = "Test string";
+    let mut var: usize = 100;
+    let var_ref: &usize = &var;
+
+    //=1=
+    //let mut text: string = "Tomek test"; // <=== can I declare string variable like this??
+    
+    let stringText = String::from("Tomek tests strings in Rust");
+    let stringText_ref: &String = &stringText; 
+
+    passByValue("Tomek tests")
+    // passByValue(text);
+    // my_other_function("Tomek test")
 }
 
 fn passByValue(parameter: String) {
     println!("{}", parameter);
 }
 
+//is it borowing???
 fn my_other_function(parameter: &str) {
     println!("{}", parameter);
 }
