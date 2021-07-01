ad291b8ba89d37424a9db29d7072234ffa4f2cb5
Tomasz Waszczyk <tomasz@waszczyk.com>
2021-07-01 22:14:19 +0200

    added house module

---
+++ modules/src/house.rs
@@ -0,0 +1,15 @@
+// mod house {
+    const HOUSE_NUMBER: u32 = 56;
+
+    pub mod bedroom1 {
+        pub fn is_light_on() -> bool {
+            false
+        }
+    }
+
+    pub mod bedroom2 {
+        pub fn is_light_on() -> bool {
+            true
+        }
+    }
+// }
