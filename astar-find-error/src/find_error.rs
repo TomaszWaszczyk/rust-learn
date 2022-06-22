ac81a0bf574a2d9bfe52524818743ae256a7b81f
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-06-22 23:52:03 +0200

    added(): astar find error

---
+++ astar-find-error/src/find_error.rs
@@ -0,0 +1,12 @@
+
+
+// find error in the code
+fn plus_five(value: i32) -> isize {
+    value + 1
+}
+
+fn main() {
+    let number = plus_five(5);
+
+    println!("The value of x is:{}", number);
+}
