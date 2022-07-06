d34287b97ae647b4cc9e8e19fea9299ec5e95e6f
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-07-06 23:51:23 +0200

    added(): nums

--- experiments/src/experiments.rs
+++ experiments/src/experiments.rs
@@ -1,8 +1,26 @@
-fn main() {
-    let var: u32 = 4;
+// fn main() {
+//     let var: u32 = 4;
+// }
+
+// fn increment(mut var: u32) -> u32 {
+//     var = var + 1;
+//     var
+// }
+
+fn bunch_of_numbers() -> Vec<u32> {
+    let mut nums = Vec::new();
+    for i in 0..10 {
+        num.push(i);
+    }
+    nums
 }
 
-fn increment(mut var: u32) -> u32 {
-    var = var + 1;
-    var
+fn main() {
+    let nums = bunch_of_numbers();
+
+    match nums.last() {
+        Some(&0) => println!("Last number is 0"),
+        Some(n) => println!("Last number is {}", n),
+        None => println!("No number was found"),
+    }
 }
