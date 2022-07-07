6aad571e6d800d58b7559f7dcad3d9cc69fd0c27
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-07-07 22:05:54 +0200

    added(experiments): dangling references

--- experiments/src/experiments.rs
+++ experiments/src/experiments.rs
@@ -6,21 +6,34 @@
 //     var = var + 1;
 //     var
 // }
+/* ************************************************************** */
+// fn bunch_of_numbers() -> Vec<u32> {
+//     let mut nums = Vec::new();
+//     for i in 0..10 {
+//         num.push(i); //(re-)allocation
+//     }
+//     nums //move
+// }
 
-fn bunch_of_numbers() -> Vec<u32> {
-    let mut nums = Vec::new();
-    for i in 0..10 {
-        num.push(i); //(re-)allocation
-    }
-    nums //move
-}
+// fn main() {
+//     let nums = bunch_of_numbers(); //obtain ownership
+
+//     match nums.last() {
+//         Some(&0) => println!("Last number is 0"),
+//         Some(n) => println!("Last number is {}", n),
+//         None => println!("No number was found"),
+//     }
+// } //deallocation
+
+/* ************************************************************** */
 
 fn main() {
-    let nums = bunch_of_numbers(); //obtain ownership
-
-    match nums.last() {
-        Some(&0) => println!("Last number is 0"),
-        Some(n) => println!("Last number is {}", n),
-        None => println!("No number was found"),
-    }
-} //deallocation
+    let r: &i32;            // ----------+-- 'a
+                            //           |
+    {                       //           |
+        let x: i32 = 5;     // -+--  'b  |
+        r = &x;             //  |        |
+    }                       // -+        |
+                            //           |
+    println!("r: {}", r);   //           |
+}                           // ----------+
