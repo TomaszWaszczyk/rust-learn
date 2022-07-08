e3c036228ea861b7223764747aba3e4e22634fa1
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-07-09 00:03:45 +0200

    added(): experiment with incrementation

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
 
-    
+    let var: u32 = 4;
+    increment(var);
 
-    println!("Hello, world!");
+
+    println!("The value of a variable is: {}", var);
+}
+
+fn increment(mut var: u32) -> u32 {
+    var += 1;
+    var
 }
