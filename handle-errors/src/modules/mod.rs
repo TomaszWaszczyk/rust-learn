3243698d1bb0fa83b2882d5dcbf36977bea5293f
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-10-26 01:24:26 +0200

    added(): modules

---
+++ handle-errors/src/modules/mod.rs
@@ -0,0 +1,13 @@
+const HOUSE_NUMBER: u32 = 56;
+
+pub mod bedroom1 {
+    pub fn is_light_on() -> bool {
+        false
+    }
+}
+
+pub mod bedroom2 {
+    pub fn is_light_on() -> bool {
+        true
+    }
+}
