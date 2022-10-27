143531b78d2f9b99d59cf12a2d0a39b7ea2d2bcb
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-10-27 17:22:26 +0200

    added(): iterators module

--- handle-errors/src/main.rs
+++ handle-errors/src/main.rs
@@ -1,10 +1,12 @@
 mod handle_errors;
 mod experiments;
 mod xor;
+mod iterators;
 
 
 fn main() {
-    xor::xor();
+    // xor::xor();
     // handle_errors::handle_errors();
     // experiments::plus_five(1);
+    iterators::iterators();
 }
