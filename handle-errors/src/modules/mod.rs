27d98191d4f117e7b98b7d591d3e4a51416bd987
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-10-27 17:39:23 +0200

    removed(): testing module

--- handle-errors/src/modules/mod.rs
+++ handle-errors/src/modules/mod.rs
@@ -11,3 +11,19 @@ pub mod bedroom2 {
         true
     }
 }
+
+mod house {
+    pub const HOUSE_NUMBER: u32 = 56;
+
+    pub mod bedroom1 {
+        pub fn is_light_on() -> bool {
+            false
+        }
+    }
+
+    pub mod bedroom2 {
+        pub fn is_light_on() -> bool {
+            true
+        }
+    }
+}
