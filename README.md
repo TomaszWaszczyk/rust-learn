95568d602d977c6104a2bdd6715fbf83ef0ca936
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-10-29 23:57:14 +0200

    added(): quiz

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 https://github.com/google/evcxr
 
-https://dtolnay.github.io/rust-quiz
-
 https://github.com/vandadnp/rust-tips-and-tricks/tree/main/tipsandtricks
 
 https://hello.kianenigma.nl/posts/for-those-who-don-t-want-rust-analyzer-one-regex-to-rul-them-all/
---
+++ src/quiz.rs
@@ -0,0 +1,37 @@
+// https://dtolnay.github.io/rust-quiz
+
+trait Trait {
+    fn f(&self);
+}
+
+impl<F: FnOnce() -> bool> Trait for F {
+    fn f(&self) {
+        print!("1");
+    }
+}
+
+impl Trait for () {
+    fn f(&self) {
+        print!("2");
+    }
+}
+
+fn main() {
+    let x = || { (return) || true; };
+    x().f();
+
+    let x = loop { (break) || true; };
+    x.f();
+
+    let x = || { return (|| true); };
+    x().f();
+
+    let x = loop { break (|| true); };
+    x.f();
+
+    let x = || { return || true; };
+    x().f();
+
+    let x = loop { break || true; };
+    x.f();
+}
