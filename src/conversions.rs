025ffdceb557d8137a8b227a5ee2a77322db3ea7
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-11-21 18:10:10 +0100

    added(conversions): daily dose of rust:from struct to other struct

--- src/conversions.rs
+++ src/conversions.rs
@@ -13,14 +13,43 @@
 
 // MAKE CONVERSION =>>
 
-pub fn make_conversions() {
-    let a: u16 = 2;
-    let b: u8 = 3;
+// pub fn make_conversions() {
+//     let a: u16 = 2;
+//     let b: u8 = 3;
 
-    let sum = add(a, b);
-    println!("Sum: {}", sum);
+//     let sum = add(a, b);
+//     println!("Sum: {}", sum);
+// }
+
+// fn add<TA: Into<f64>, TB: Into<f64>>(a: TA, b: TB) -> f64 {
+//     a.into() + b.into()
+// }
+
+struct MyStruct {
+    pub my_data: String,
 }
 
-fn add<TA: Into<f64>, TB: Into<f64>>(a: TA, b: TB) -> f64 {
-    a.into() + b.into()
+#[derive(Clone)]
+struct MyOtherStruct {
+    pub my_other_data: String,
+}
+
+impl From<MyOtherStruct> for MyStruct {
+    fn from(my_other_struct: MyOtherStruct) -> Self {
+        MyStruct {
+            my_data: my_other_struct.my_other_data,
+        }
+    }
+}
+
+fn main() {
+    let my_other_struct = MyOtherStruct {
+        my_other_data: "some text".to_string(),
+    };
+
+    let my_struct: MyStruct = MyStruct::from(my_other_struct.clone());
+
+    let my_second_struct: MyStruct = my_other_struct.into();
+
+    assert_eq!(my_struct.my_data, my_second_struct.my_data);
 }
