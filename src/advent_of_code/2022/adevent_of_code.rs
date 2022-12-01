726427e62dd7afcfd0142c131965b9c83987b734
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-12-01 14:28:20 +0100

    added(): advent of code

---
+++ src/advent_of_code/2022/adevent_of_code.rs
@@ -0,0 +1,21 @@
+mod day_01 {
+    pub fn count () -> Result<Vec<u128>> {
+        // let (mut max, buf) = 
+        //     std::io::stdin()
+        //         .lock()
+        //         .lines()
+        //         .fold(Ok(()))
+    }
+}
+
+mod day_02 {
+    pub fn hello_future() {
+        println!("Hello future");
+    }
+}
+
+fn main() {
+    use day_01::hello;
+
+    hello();
+}
