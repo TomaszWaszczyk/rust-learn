ce26c66eb742d7ff55d151f50c45b8662f6a689a
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-12-05 13:43:34 +0100

    added(experiments): currying

--- src/experiments/mod.rs
+++ src/experiments/mod.rs
@@ -8,3 +8,27 @@ use std::convert::TryInto;
 pub fn plus_five(value: i32) -> isize {
     (value + 1).try_into().unwrap()
 }
+
+pub mod currying {
+    // Currying transforms a function with multiple arguments into functions that only take one.
+    // It can be achieved in Rust using closures because of their ability to capture variables 
+    // from their defined scope. It's a pattern often used in functional programming 
+    // but not one I have seen used much in Rust.
+
+    // 👉 Currying can help when you have to call a function with a fixed argument frequently
+    // 👉 It's a way to produce high-order functions that contain context 
+    // 👉 Can make the function calls easier to read because of fewer arguments
+    pub fn multiply_curry(number: i32) -> impl Fn(i32) -> i32 {
+        move |x| number * x
+    }
+}
+
+fn main() {
+    let double = currying::multiply_curry(2);
+
+    let double_four = double(4);
+    let double_five = double(5);
+
+    assert_eq!(double_four, 8);
+    assert_eq!(double_five, 10);
+}
