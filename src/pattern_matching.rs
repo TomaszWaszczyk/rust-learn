12d538a18c64d64295f1c4e1fe02ba9783621474
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-02-21 00:23:58 +0100

    added(pattern_matching): comments and tests

--- src/pattern_matching.rs
+++ src/pattern_matching.rs
@@ -1,3 +1,8 @@
+// 🛡️ Use pattern matching with if let and let else statements
+// The if let and let else statements in Rust are super helpful when matching values when we don't want to be exhaustive like in the match operator.
+// You can combine these statements with pattern matching, like in the match operator.
+// It can be helpful if you, for example, want to match two enum variants when there are many values or match on an inclusive range.
+
 enum MyEnum {
     MyValue(i32),
     MyOtherValue(i32),
@@ -21,5 +26,7 @@ fn match_inclusive_range(number: i32) -> bool {
 fn main() {
     assert_eq!(match_multiple_values(MyEnum::MyValue(2)), 2);
     assert_eq!(match_multiple_values(MyEnum::MyOtherValue(2)), 2);
-
+    assert_eq!(match_inclusive_range(5), true);
+    assert_eq!(match_inclusive_range(6), true);
+    assert_eq!(match_inclusive_range(7), false);
 }
