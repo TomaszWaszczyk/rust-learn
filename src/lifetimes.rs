273aae5f1124e97b6f00d8fdc3f6fdff49d02421
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-02-27 00:39:19 +0100

    added(): 'lifetime

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -5,7 +5,7 @@ fn main() {
 // 2. If there is exactly one input lifetime parameter, that lifetime parameter is assigned to all outputs lifetime parameters;
 // 3. If there are multiple input lifetime parameters, but one of them is &self or &mut self the lifetime of self is assigned to all output lifetime parameters.
 
-fn first_word(s: &str) -> &str {
+fn first_word<'lifetime>(s: &'lifetime str) -> &'lifetime str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
