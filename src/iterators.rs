0ab9fdab0a71333bd98f7df9f397cdba8f21cf3d
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-03-06 01:12:03 +0100

    added(iterators): ehh

--- src/iterators.rs
+++ src/iterators.rs
@@ -10,3 +10,13 @@ pub fn iterators() {
     let _iter = (left_limes..right_limes).filter(|x| x % 2 == 0);
     println!("{}", v.len());
 }
+
+fn main() {
+    let v = [1, 2, 3];
+    let mut iter = v.into_iter();
+    
+    assert_eq!(Some(1), iter.next());
+    assert_eq!(Some(2), iter.next());
+    assert_eq!(Some(3), iter.next());
+    assert_eq!(None, iter.next());
+}
