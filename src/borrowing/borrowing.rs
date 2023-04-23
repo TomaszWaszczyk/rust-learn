6ef4b085bc0233a88d706c17a3e2afe074b7df33
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-04-23 21:04:05 +0200

    added(): references with tuples and arrays

--- src/borrowing/borrowing.rs
+++ src/borrowing/borrowing.rs
@@ -3,8 +3,8 @@ struct Foo {}
 fn test(in1: &mut Foo, in2: &mut Foo) {}
 
 fn main() {
-    let mut out1 = [Foo{}, Foo{}];
-    let mut out2 = (Foo{}, Foo{});
+    let mut out1 = [Foo {}, Foo {}];
+    let mut out2 = (Foo {}, Foo {});
 
     test(&mut out1[0], &out1[1]);
     test(&mut out2.0, &out2.1);
