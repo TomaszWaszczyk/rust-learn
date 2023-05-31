a0ae43ea9c9c342212959d01713c25db4976e135
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-05-31 18:13:05 +0200

    added(daily_rust): linked list without tests

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -13,5 +13,28 @@ mod linked_list {
         fn new(value: T) -> Self {
             LinkedList{ head: None }
         }
+
+        fn push(&mut self, _value: T) {
+            let prev_head = self.head.take();
+
+            let new_head = match prev_head {
+                None => Node { value: _value, next: None },
+                Some(prev_head) => Node { value: _value, next: Some(Box::new(prev_head)) }
+            };
+
+            self.head = Some(new_head);
+        }
+
+        fn pop(&mut self) -> Option<&T> {
+            let prev_head = self.head.take();
+
+            let new_head = match prev_head {
+                None => _,
+                Some(node) => Some(*node)
+            };
+
+            self.head = new_head;
+            Some(&old_head.value);
+        }
     }
 }
