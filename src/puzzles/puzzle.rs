0b5fbd100ca17c71539b10cbf48c37fa6edf68ca
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-08-20 17:19:24 +0200

    add(): link

--- src/puzzles/puzzle.rs
+++ src/puzzles/puzzle.rs
@@ -1,9 +1,11 @@
+// https://stackoverflow.com/questions/48015600/cannot-use-operator-for-functions-that-return-result-error
+
 fn main() {
     let a = some_complex_function()?;
 
     println!("{:?}", a);
 }
 
-fn some_complex_function<T>() -> Option<T> {
-    Some(())
+fn some_complex_function<T, E>() -> Result<T, E> {
+    // Ok(Ok("complex"))
 }
