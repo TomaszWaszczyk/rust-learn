ab3c4ee179ac5fa8afe78e7edd977bad23a91ef9
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-09-03 17:29:48 +0200

    add(new): test

--- src/dynamic-dispatch/mod.rs
+++ src/dynamic-dispatch/mod.rs
@@ -10,6 +10,24 @@ trait Spellchecker {
     fn check(&self, input: &str) -> Vec<Change>;
 }
 
+struct CapitalLetterSpellChecker;
+
+impl Spellchecker for AntispaceChecker {
+    fn check(&self, input: &str) -> Vec<Change> {
+        input
+            .match_indices(" ")
+            .map(|(index, space)| {
+                Change::Delete(index..index + space.len())
+            }).collect()
+    }
+}
+
+impl Spellchecker for NoopSpellchecker {
+    fn check(&self, input: &str) -> Vec<Change> {
+        Vec::new()
+    }
+}
+
 fn apply_change(string: &mut String, change: Change) {
 
 }
@@ -17,4 +35,17 @@ fn apply_change(string: &mut String, change: Change) {
 enum Change {
     Delete(std::ops::Range<usize>),
     Replace(std::ops::Range<usize>, String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let text = "Hello world, I know you are looking at me.";
+        let result = spellcheck(text, NoopSpellchecker);
+        assert!(result == test);
+        let result = spellcheck(text, AntispaceChecker);
+    }
 }
