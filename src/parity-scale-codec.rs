7ea654e878f97588c1b8dcdaaa6fbcab89ee2320
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-09-11 03:05:38 +0200

    add(tinkering): parity scale codec

---
+++ src/parity-scale-codec.rs
@@ -0,0 +1,10 @@
+extern crate parity_scale_codec;
+
+use parity_scale_codec::{Encode, Decode};
+
+fn main() {
+    // println!("{:02x?}", 69i8.encode());
+    // println!("{:02x?}", 69u8.encode());
+    // println!("{:02x?}", 42u16.encode());
+    // println!("{:02x?}", 16777215u32.encode());
+}
