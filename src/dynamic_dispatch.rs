16ca08e7aeccdb2feac1f0a79c7252b404ff90d5
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-09-12 13:40:43 +0200

    add(tinkering): dynamic dispatch

---
+++ src/dynamic_dispatch.rs
@@ -0,0 +1,33 @@
+struct Circle {
+    radius: f64
+}
+
+struct Square {
+    side: f64
+}
+
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+impl Shape for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        self.radius * self.radius * std::f64::consts::PI
+    }
+}
+
+fn main() {
+    // trait objects must include the `dyn` keyword
+    let shapes: [&dyn Shape; 2] = [&Circle{radius: 1.3}, &Square{side:2.0}];
+
+    // &&dyn Shape
+    for (i, shape) in shapes.iter().enumerate() {
+        println!("Shape #{} has area {}", i, shape.area());
+    }
+}
